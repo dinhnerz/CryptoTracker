989eb78bc9a1ada3166b91b3f04d96f8bcca0ab5
Dinh Luong <33304720+dinhnerz@users.noreply.github.com>
2018-01-03 14:22:01 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,99 @@
-/*
-** file: js/main.js
-** description: javascript code for "html/main.html" page
-*/
+// dinhnluong@gmail.com 01.03.2018
 
-function init_main () {
-    $('html').hide().fadeIn('slow');
-}
+var tableHTML = "";
 
-//bind events to dom elements
-document.addEventListener('DOMContentLoaded', init_main);
+var storageCache = {};
+var apiArray = [];
+
+var grab = "bitcoin";
+
+
+$(document).ready(function() { 
+
+  chrome.storage.sync.get(null, function(data) {
+
+    storageCache = data;
+
+    console.log(storageCache)
+
+    if (storageCache.bitcoin == "on") {
+      apiArray.push("bitcoin");
+    }
+    if (storageCache.litecoin == "on") {
+      apiArray.push("litecoin");
+    }
+    if (storageCache.ripple == "on") {
+      apiArray.push("ripple");
+    }
+    if (storageCache.ethereum == "on") {
+      apiArray.push("ethereum");
+    }
+    if (storageCache.cardano == "on") {
+      apiArray.push("cardano");
+    }
+    if (storageCache.iota == "on") {
+      apiArray.push("iota");
+    }
+    if (storageCache.tron == "on") {
+      apiArray.push("tron");
+    }
+    if (storageCache.stellar == "on") {
+      apiArray.push("stellar");
+    }
+    if (storageCache.bitcoincash == "on") {
+      apiArray.push("bitcoin-cash");
+    }
+    if (storageCache.qtum == "on") {
+      apiArray.push("qtum");
+    }
+    if (storageCache.bitcoingold == "on") {
+      apiArray.push("bitcoin-gold");
+    }
+    if (storageCache.eos == "on") {
+      apiArray.push("eos");
+    }
+    if (storageCache.neo == "on") {
+      apiArray.push("neo");
+    }
+    if (storageCache.dash == "on") {
+      apiArray.push("dash");
+    }
+    if (storageCache.monero == "on") {
+      apiArray.push("monero");
+    }
+    if (storageCache.nem == "on") {
+      apiArray.push("nem");
+    }
+
+    console.log(apiArray[0]);
+
+    for (i = 0; i < apiArray.length; i++) {
+      $.getJSON("https://api.coinmarketcap.com/v1/ticker/" + apiArray[i] + "/", function (data) {
+
+        console.log(data);
+
+        tableHTML = "";
+
+        tableHTML += "<tr><td><img src=\"../images/logo/" + data[0].id + ".png\"></td>";
+        tableHTML += "<td>" + data[0].symbol + "</td>";
+        tableHTML += "<td>" + data[0].name +"</td>";
+        tableHTML += "<td>$" + data[0].price_usd + "</td>";
+        tableHTML += "<td>" + data[0].percent_change_24h + "</td><tr>";
+
+        $("#cryptoTable").append(tableHTML);
+        console.log("hello");
+
+
+      });
+
+    }
+
+
+  });
+
+
+  
+});
+
+
+
--- js/options.js
+++ js/options.js
@@ -1,37 +1,413 @@
-/*
-** file: js/options.js
-** description: javascript code for "html/options.html" page
-*/
+// dinhnluong@gmail.com 01.03.2018
 
-function init_options () {
-    console.log("function: init_options");
 
-    //load currently stored options configuration
-    var favorite_movie = localStorage['favorite_movie'];
+// clear storage for debugging only, activate by removing comments
+// chrome.storage.sync.clear(); 
 
-    //set the current state of the options form elements to match the stored options values
-    //favorite_movie
-    if (favorite_movie) {
-        var favorite_movie_dropdown = document.getElementById('favorite-movie-dropdown');
-        for (var i=0; i < favorite_movie_dropdown.children.length; i++) {
-            var option = favorite_movie_dropdown.children[i];
-            if (option.value == favorite_movie) {
-                option.selected = 'true';
-                break;
-            }
-        }
+var storageCache = {}; //create a object to store chrome.storage
+
+
+// storage.sync will sync if user is logged in onto another chrome browser it will load the sync information from the user
+// will act as a storage.local if user is not synced
+
+// Save it using the Chrome extension storage API.
+
+
+// check chrome storage to see we have anything saved, if not then initalate default 5 coins
+
+chrome.storage.sync.get("default", function(result) {
+
+    if (result.default == null) { // confirmed default
+
+        // set default 5 coins
+
+        chrome.storage.sync.set({"bitcoin": "on"}, null);
+        chrome.storage.sync.set({"litecoin": "on"}, null);
+        chrome.storage.sync.set({"ethereum": "on"}, null);
+        chrome.storage.sync.set({"ripple": "on"}, null);
+        chrome.storage.sync.set({"bitcoincash": "on"}, null);
+        chrome.storage.sync.set({"cardano": "off"}, null);
+        chrome.storage.sync.set({"tron": "off"}, null);
+        chrome.storage.sync.set({"iota": "off"}, null);
+        chrome.storage.sync.set({"verge": "off"}, null);
+        chrome.storage.sync.set({"stellar": "off"}, null);
+        chrome.storage.sync.set({"nem": "off"}, null);
+        chrome.storage.sync.set({"dash": "off"}, null);
+        chrome.storage.sync.set({"monero": "off"}, null);
+        chrome.storage.sync.set({"neo": "off"}, null);
+        chrome.storage.sync.set({"eos": "off"}, null);
+        chrome.storage.sync.set({"bitcoingold": "off"}, null);
+        chrome.storage.sync.set({"qtum": "off"}, null);
+
+
+        chrome.storage.sync.set({"default": "false"}, null);
+
+        console.log("we are in default");
+
+        checkFavorites();
+
+    } else if (result.default == "false") {
+
+        console.log("no default");
+
+        checkFavorites();
+
     }
+
+});
+
+
+
+function checkFavorites() {
+
+    chrome.storage.sync.get(null, function(data) {
+        storageCache = data;
+        console.log(storageCache);
+        
+        // if coins is on make sure the tab is a grey color
+
+        if (storageCache.bitcoin == "on") {
+            document.getElementById("bitcoin").style.background = "#D3D3D3";
+        }
+        if (storageCache.litecoin == "on") {
+            document.getElementById("litecoin").style.background = "#D3D3D3";
+        }
+        if (storageCache.ripple == "on") {
+            document.getElementById("ripple").style.background = "#D3D3D3";
+        }
+        if (storageCache.ethereum == "on") {
+            document.getElementById("ethereum").style.background = "#D3D3D3";
+        }
+        if (storageCache.cardano == "on") {
+            document.getElementById("cardano").style.background = "#D3D3D3";
+        }
+        if (storageCache.iota == "on") {
+            document.getElementById("iota").style.background = "#D3D3D3";
+        }
+        if (storageCache.tron == "on") {
+            document.getElementById("tron").style.background = "#D3D3D3";
+        }
+        if (storageCache.stellar == "on") {
+            document.getElementById("stellar").style.background = "#D3D3D3";
+        }
+        if (storageCache.bitcoincash == "on") {
+            document.getElementById("bitcoincash").style.background = "#D3D3D3";
+        }
+        if (storageCache.stellar == "on") {
+            document.getElementById("stellar").style.background = "#D3D3D3";
+        }
+        if (storageCache.nem == "on") {
+            document.getElementById("nem").style.background = "#D3D3D3";
+        }
+        if (storageCache.dash == "on") {
+            document.getElementById("dash").style.background = "#D3D3D3";
+        }
+        if (storageCache.monero == "on") {
+            document.getElementById("monero").style.background = "#D3D3D3";
+        }
+        if (storageCache.neo == "on") {
+            document.getElementById("neo").style.background = "#D3D3D3";
+        }
+        if (storageCache.eos == "on") {
+            document.getElementById("eos").style.background = "#D3D3D3";
+        }
+        if (storageCache.bitcoingold == "on") {
+            document.getElementById("bitcoingold").style.background = "#D3D3D3";
+        }
+        if (storageCache.qtum == "on") {
+            document.getElementById("qtum").style.background = "#D3D3D3";
+        }
+
+    });
+
+
+
+
+
 }
 
-function save_options () {
-    console.log("function: save_options");
 
-    //favorite-movie-dropdown
-    var favorite_movie = document.getElementById('favorite-movie-dropdown').children[document.getElementById('favorite-movie-dropdown').selectedIndex].value;
-    localStorage['favorite_movie'] = favorite_movie;
-    console.log("favorite_movie = " + favorite_movie);
-}
+$(".ethereum").click(function(){
 
-//bind events to dom elements
-document.addEventListener('DOMContentLoaded', init_options);
-document.querySelector('#save-options-button').addEventListener('click', save_options);
+    chrome.storage.sync.get("ethereum", function(result) {
+
+        var data = result.ethereum;
+
+        if (data == "off") {
+           document.getElementById("ethereum").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"ethereum": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("ethereum").style.background = "";
+           chrome.storage.sync.set({"ethereum": "off"}, null);
+       }
+
+   });
+
+});
+
+
+$(".tron").click(function(){
+
+    chrome.storage.sync.get("tron", function(result) {
+
+        var data = result.tron;
+
+        if (data == "off") {
+           document.getElementById("tron").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"tron": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("tron").style.background = "";
+           chrome.storage.sync.set({"tron": "off"}, null);
+       }
+
+   });
+
+});
+
+
+$(".litecoin").click(function(){
+
+    chrome.storage.sync.get("litecoin", function(result) {
+
+        var data = result.litecoin;
+
+        if (data == "off") {
+           document.getElementById("litecoin").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"litecoin": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("litecoin").style.background = "";
+           chrome.storage.sync.set({"litecoin": "off"}, null);
+       }
+
+   });
+});
+
+
+$(".bitcoin").click(function(){
+
+    chrome.storage.sync.get("bitcoin", function(result) {
+
+        var data = result.bitcoin;
+
+        if (data == "off") {
+           document.getElementById("bitcoin").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"bitcoin": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("bitcoin").style.background = "";
+           chrome.storage.sync.set({"bitcoin": "off"}, null);
+       }
+
+   });
+
+});
+
+$(".bitcoincash").click(function(){
+
+    chrome.storage.sync.get("bitcoincash", function(result) {
+
+        var data = result.bitcoincash;
+
+        if (data == "off") {
+           document.getElementById("bitcoincash").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"bitcoincash": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("bitcoincash").style.background = "";
+           chrome.storage.sync.set({"bitcoincash": "off"}, null);
+       }
+
+   });
+});
+
+$(".cardano").click(function(){
+
+    chrome.storage.sync.get("cardano", function(result) {
+
+        var data = result.cardano;
+
+        if (data == "off") {
+           document.getElementById("cardano").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"cardano": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("cardano").style.background = "";
+           chrome.storage.sync.set({"cardano": "off"}, null);
+       }
+
+   });
+});
+
+$(".iota").click(function(){
+
+    chrome.storage.sync.get("iota", function(result) {
+
+        var data = result.iota;
+
+        if (data == "off") {
+           document.getElementById("iota").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"iota": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("iota").style.background = "";
+           chrome.storage.sync.set({"iota": "off"}, null);
+       }
+
+   });
+});
+
+$(".ripple").click(function(){
+    chrome.storage.sync.get("ripple", function(result) {
+
+        var data = result.ripple;
+
+        if (data == "off") {
+           document.getElementById("ripple").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"ripple": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("ripple").style.background = "";
+           chrome.storage.sync.set({"ripple": "off"}, null);
+       }
+
+   });
+});
+
+$(".stellar").click(function(){
+    chrome.storage.sync.get("stellar", function(result) {
+
+        var data = result.stellar;
+
+        if (data == "off") {
+           document.getElementById("stellar").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"stellar": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("stellar").style.background = "";
+           chrome.storage.sync.set({"stellar": "off"}, null);
+       }
+
+   });
+});
+
+$(".nem").click(function(){
+    chrome.storage.sync.get("nem", function(result) {
+
+        var data = result.nem;
+
+        if (data == "off") {
+           document.getElementById("nem").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"nem": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("nem").style.background = "";
+           chrome.storage.sync.set({"nem": "off"}, null);
+       }
+
+   });
+});
+
+$(".dash").click(function(){
+    chrome.storage.sync.get("dash", function(result) {
+
+        var data = result.dash;
+
+        if (data == "off") {
+           document.getElementById("dash").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"dash": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("dash").style.background = "";
+           chrome.storage.sync.set({"dash": "off"}, null);
+       }
+
+   });
+});
+
+$(".monero").click(function(){
+    chrome.storage.sync.get("monero", function(result) {
+
+        var data = result.monero;
+
+        if (data == "off") {
+           document.getElementById("monero").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"monero": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("monero").style.background = "";
+           chrome.storage.sync.set({"monero": "off"}, null);
+       }
+
+   });
+});
+
+$(".neo").click(function(){
+    chrome.storage.sync.get("neo", function(result) {
+
+        var data = result.neo;
+
+        if (data == "off") {
+           document.getElementById("neo").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"neo": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("neo").style.background = "";
+           chrome.storage.sync.set({"neo": "off"}, null);
+       }
+
+   });
+});
+
+$(".eos").click(function(){
+    chrome.storage.sync.get("eos", function(result) {
+
+        var data = result.eos;
+
+        if (data == "off") {
+           document.getElementById("eos").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"eos": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("eos").style.background = "";
+           chrome.storage.sync.set({"eos": "off"}, null);
+       }
+
+   });
+});
+
+$(".bitcoingold").click(function(){
+    chrome.storage.sync.get("bitcoingold", function(result) {
+
+        var data = result.bitcoingold;
+
+        if (data == "off") {
+           document.getElementById("bitcoingold").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"bitcoingold": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("bitcoingold").style.background = "";
+           chrome.storage.sync.set({"bitcoingold": "off"}, null);
+       }
+
+   });
+});
+
+$(".qtum").click(function(){
+    chrome.storage.sync.get("qtum", function(result) {
+
+        var data = result.qtum;
+
+        if (data == "off") {
+           document.getElementById("qtum").style.background = "#D3D3D3";
+           chrome.storage.sync.set({"qtum": "on"}, null);
+
+       } else if (data == "on") {
+           document.getElementById("qtum").style.background = "";
+           chrome.storage.sync.set({"qtum": "off"}, null);
+       }
+
+   });
+});
