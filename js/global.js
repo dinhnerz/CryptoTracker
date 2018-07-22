9781dc5be842d3c9066573e1a0e561e2e004d90b
Dinh Luong <33304720+dinhnerz@users.noreply.github.com>
2018-07-22 15:57:14 -0700

    Delete global.js

--- js/global.js
+++
@@ -1,5 +0,0 @@
-/*
-** file: js/global.js
-** description: global javascript library containing code for use across the entire chrome extension, for
-**              unique functionality for specific pages, create and use a separate file named "pagename.js"
-*/
